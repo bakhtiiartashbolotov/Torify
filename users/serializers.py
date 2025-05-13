6aaafeac07dcfacb3b03d9b654da0ef81d5f8453
bakhtiiartashbolotov <bakhtiiar.tashbolotov@alatoo.edu.kg>
2025-05-13 15:33:46 +0600

    Revert "смотри"
    This reverts commit 3971e81127dd2b539ea57f94f52a252b28ac05c9.

--- users/serializers.py
+++ users/serializers.py
@@ -1,40 +1,27 @@
-# users/serializers.py
-
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    # Поле для ввода пароля (без возвращения в ответе)
     password = serializers.CharField(write_only=True, style={'input_type': 'password'})
-    # Поле роли (используем choices из модели)
     role = serializers.ChoiceField(choices=User._meta.get_field('role').choices)
 
     class Meta:
         model = User
-        # Поля, которые клиент может передавать при регистрации
         fields = ('id', 'full_name', 'phone_number', 'email', 'password', 'role')
-        extra_kwargs = {
-            'password': {'write_only': True},
-        }
 
     def create(self, validated_data):
-        # Создаём экземпляр User напрямую, без передачи username
-        user = User(
-            phone_number=validated_data['phone_number'],
+        user = User.objects.create_user(
             full_name=validated_data['full_name'],
+            phone_number=validated_data['phone_number'],
             email=validated_data['email'],
-            role=validated_data['role'],
+            password=validated_data['password'],
+            role=validated_data['role']
         )
-        # Устанавливаем пароль (хэшируется внутри модели)
-        user.set_password(validated_data['password'])
-        # Сохраняем пользователя в БД
-        user.save()
         return user
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        # Поля, которые возвращаем клиенту при запросах профиля и т.п.
         fields = ('id', 'full_name', 'phone_number', 'email', 'role')
